39e80558af9a98352154c9ad97fe40a8434a80a8
naa-adjorkor <naakrong_adjei@st.ug.edu.gh>
2024-07-02 03:10:20 +0000

    added subheader section in HomeScreen

--- myProject/screens/HomeScreen.jsx
+++ myProject/screens/HomeScreen.jsx
@@ -10,6 +10,11 @@ export default function HomeScreen() {
         <Image source={require('./../assets/images/Search.png')} style={styles.search}/>
         <Pressable><Image source={require('./../assets/images/shoppingBag.png')} style={styles.bag}/></Pressable>
       </View>
+      <View style={styles.subHeader}>
+        <Text style={styles.text}>OUR STORY</Text>
+        <Pressable style={styles.list}><Image source={require('./../assets/images/Listview.png')} style={styles.listIcon}/></Pressable>
+        <Pressable style={styles.filter}><Image source={require('./../assets/images/Filter.png')} style={styles.filterIcon}/></Pressable>
+      </View>
     </View>
   )
 }
@@ -39,5 +44,40 @@ const styles = StyleSheet.create({
     width:30,
     height:30,
     right:55,
+  },
+  subHeader:{
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center',
+    top:30,
+  },
+  text:{
+    fontSize:22,
+    fontWeight:'300',
+    color:'#000000',
+    letterSpacing:2.5,
+  },
+  list:{
+    padding:15,
+    borderRadius:30,
+    left:35,
+    backgroundColor:'#E8E8E8'
+  },
+  filter:{
+    padding:15,
+    borderRadius:30,
+    backgroundColor:'#E8E8E8',
+    right:42,
+  },
+  listIcon:{
+    height:26,
+    width:26,
+  },
+  filterIcon:{
+    height:26,
+    width:26,
   }
+  
+  
+
 })
